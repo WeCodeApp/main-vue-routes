0d2955b3544c029828530fb74a7165db4714c28a
shairacruz <shairayvonnecruz028@gmail.com>
2025-04-27 06:05:40 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit dolore saepe quidem doloribus, voluptates est, repellat deleniti quia enim odio, qui odit laboriosam eligendi perferendis alias modi reiciendis. Nulla, libero corporis recusandae explicabo asperiores perspiciatis autem voluptatem facilis, perferendis laudantium dolor quis eligendi fuga, at animi. Quos qui quibusdam veniam enim est modi, magnam at earum velit distinctio officia repellendus iure veritatis, nemo animi cupiditate deserunt mollitia neque, assumenda explicabo. Aliquid, ullam sequi eos id eligendi eum officiis! Nam, quo amet. Quod perferendis exercitationem repudiandae, odio voluptatem cumque labore non fugiat repellat saepe, aliquam est perspiciatis magnam omnis assumenda numquam.</p>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Job Details</h1>
+    <p>This is job {{ id }}</p>
+</template>
+
+<script>
+export default {
+    props: ['id']
+}
+</script>
